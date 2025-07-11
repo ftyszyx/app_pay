0b782880917b9319e3f348b94abd099ad2c087d6
ftyszyx <whyzi@qq.com>
2025-07-11 13:57:21 +0800

    add logs

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,13 +3,15 @@ use axum::{
     response::Html,
     routing::{get, post},
 };
+use chrono::{FixedOffset, Utc};
 use migration::{Migrator, MigratorTrait};
 use std::{env, net::SocketAddr};
 use tower_http::{
     cors::{Any, CorsLayer},
     trace::TraceLayer,
 };
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_appender::rolling;
+use tracing_subscriber::{fmt::time::FormatTime, layer::SubscriberExt, util::SubscriberInitExt};
 use utoipa::{
     Modify, OpenApi,
     openapi::security::{ApiKey, ApiKeyValue, SecurityScheme},
@@ -21,6 +23,16 @@ use crate::handlers::middleware::auth;
 mod database;
 mod entities;
 mod handlers;
+
+struct East8Timer;
+
+impl FormatTime for East8Timer {
+    fn format_time(&self, w: &mut tracing_subscriber::fmt::format::Writer<'_>) -> std::fmt::Result {
+        let east8 = FixedOffset::east_opt(8 * 3600).unwrap();
+        let now = Utc::now().with_timezone(&east8);
+        write!(w, "{}", now.format("%Y-%m-%dT%H:%M:%S"))
+    }
+}
 
 #[derive(OpenApi)]
 #[openapi(
@@ -55,12 +67,20 @@ impl Modify for SecurityAddon {
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
+    let file_appender = rolling::daily("logs", "app.log");
+    let (non_blocking_appender, _guard) = tracing_appender::non_blocking(file_appender);
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
                 .unwrap_or_else(|_| "app_server=debug,tower_http=debug".into()),
         )
-        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::fmt::layer().with_timer(East8Timer))
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_ansi(false)
+                .with_timer(East8Timer)
+                .with_writer(non_blocking_appender),
+        )
         .init();
 
     let db_pool = database::init_db()
@@ -73,12 +93,14 @@ async fn main() {
         .route("/products", get(handlers::product::get_products))
         .route_layer(middleware::from_fn(auth));
 
+    // build our application with a route
     let app = Router::new()
         .route("/", get(handler))
-        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
         .route("/api/register", post(handlers::auth::register))
         .route("/api/login", post(handlers::auth::login))
         .nest("/api/admin", admin_routes)
+        // add swagger ui
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
         .with_state(db_pool)
         .layer(cors)
         .layer(TraceLayer::new_for_http());
