f9fa0226b78d3ba7e16bbd02c1c5e7885af8d638
ftyszyx <whyzi@qq.com>
2025-09-28 23:20:07 +0800

    fix

--- pub/update_all.sh
+++ pub/update_all.sh
@@ -1,4 +1,4 @@
 git pull --rebase
-cp -rf web/* /opt/1panel/www/sites/apps.bytefuse.cn/index/*
+rsync -avz web/ /opt/1panel/www/sites/apps.bytefuse.cn/index/ 
 docker compose -f docker-compose.release.yml down server
 docker compose -f docker-compose.release.yml up -d --force-recreate server
